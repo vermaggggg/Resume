ea431d03037c297fb5e00bf89523f12a384ab2a6
Anil Kumar <anil.kumar@pizoneinfotech.com>
2023-09-27 10:23:15 +0530

    2.0 button remove

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,13 @@ import Container from 'react-bootstrap/Container';
 
 function Footer() {
   return (
+    <>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5499182087357803"
+     crossorigin="anonymous"></script>
     <Container fluid  className="border-top mt-2 text-center text-dark py-3 bottom_bg">
         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/anil-verma-028b73210/" className='bottom_sec'>Anil Verma</a>
     </Container>
+    </>
   )
 }
 
